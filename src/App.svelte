b659503f38a3fe4850d6538c2fb24dc2f1a19078
takemeto404 <takemeto404@gmail.com>
2024-02-29 15:58:05 +0300

    done load start topY depending on disk size

--- src/App.svelte
+++ src/App.svelte
@@ -2,47 +2,36 @@
     import { onMount } from 'svelte'
     import { tweened } from 'svelte/motion'
     import { readable, writable, get } from 'svelte/store'
-    import { width, height, scale} from './sizes'
+    import { width, height, scale } from './sizes'
 
     const containerHeightPx: number = 700
-    const bigDiskDiameterPx: number = 300
-    const smallDiskDiameterPx = 70
+    const bigDiskDiameterPx: number = 200
+    const smallDiskDiameterPx = 40
     const loadDiameterPx = 80
-
-    const smallDiskPositionY = bigDiskDiameterPx / 2 - smallDiskDiameterPx / 2
-
-    let loadPositionPx = bigDiskDiameterPx / 2
-    const loadFallingSpeedPx = tweened(0)
-
-    let disksRotation = 0
-    const disksRotationSpeed = tweened(0)
 
     const g = 9.81
     let m = .6
     let d = .07 // in meters
-    let n1 = 4
+    let n1 = 1
 
     let n2 = 20
     let t = 4 // in seconds
 
     let H = n1 * 2 * Math.PI * d // in meters
 
-    let bigDiskElement
-    let smallDiskElement
+    let loadPositionPx = containerHeightPx - (loadDiameterPx + n1 * smallDiskDiameterPx * Math.PI)
+    const loadFallingSpeedPx = tweened(0)
+
+    let disksRotation = 0
+    const disksRotationSpeed = tweened(0)
 
     onMount(async function loadFalling() {
 
-        const loadBeginTopPx = bigDiskDiameterPx / 2
+        const loadBeginTopPx = containerHeightPx - (loadDiameterPx + n1 * smallDiskDiameterPx * Math.PI)
         const loadEndTopPx = containerHeightPx - loadDiameterPx
-
-        const loadBeginSpeed = 0
-        const loadEndSpeed = 2 * H / t
 
         const loadBeginSpeedPx = 0
         const loadEndSpeedPx = 2 * (loadEndTopPx - loadBeginTopPx) / t
-
-        const circlesBeginRotation = 0
-        const circlesEndRotation = n1
 
         const circlesBeginRotationSpeed = 0
         const circlesEndRotationSpeed = 2 * n1 / t
@@ -67,8 +56,6 @@
             const now = performance.now()
             const deltaSec = (now - lastUpdate) / 1000
 
-            // console.log(deltaSec)
-
             loadPositionPx += get(loadFallingSpeedPx) * deltaSec
             disksRotation += get(disksRotationSpeed) * deltaSec
 
@@ -82,17 +69,17 @@
 </script>
 
 <div class="app" style="width: {width}px; height: {height}px; --scale: {$scale};">
-<!--    <div class="toolbar">-->
-<!--        <input bind:value={m} placeholder="Масса груза(г)...">-->
-<!--        <input bind:value={d} placeholder="Диаметр диска...">-->
-<!--        <input bind:value={n1} placeholder="Количество оборотов...">-->
-<!--    </div>-->
+    <!--    <div class="toolbar">-->
+    <!--        <input bind:value={m} placeholder="Масса груза(г)...">-->
+    <!--        <input bind:value={d} placeholder="Диаметр диска...">-->
+    <!--        <input bind:value={n1} placeholder="Количество оборотов...">-->
+    <!--    </div>-->
 
     <div class="physics-container" style="height: {containerHeightPx}px; width: {bigDiskDiameterPx}px;">
-        <div class="big-disk disk" bind:this={bigDiskElement}
+        <div class="big-disk disk"
              style="width: {bigDiskDiameterPx}px; transform: rotateZ({disksRotation * 360}deg);"></div>
-        <div class="small-disk disk" bind:this={smallDiskElement}
-             style="width: {smallDiskDiameterPx}px; top: {smallDiskPositionY}px; left: {smallDiskPositionY}px; transform: rotateZ({disksRotation * 360}deg);"></div>
+        <div class="small-disk disk"
+             style="width: {smallDiskDiameterPx}px; top: {bigDiskDiameterPx / 2 - smallDiskDiameterPx / 2}px; left: {bigDiskDiameterPx / 2 - smallDiskDiameterPx / 2}px; transform: rotateZ({disksRotation * 360}deg);"></div>
         <div class="load"
              style="width: {loadDiameterPx}px; height: {loadDiameterPx}px; left: {(bigDiskDiameterPx + smallDiskDiameterPx - loadDiameterPx) / 2}px; top: {loadPositionPx}px;"></div>
     </div>
@@ -105,7 +92,6 @@
         left: 50%;
         transform: translateX(-50%) translateY(-50%) scale(var(--scale));
         border: 1px solid black;
-        background-image: url("./assets/bg.jpeg");
         background-size: cover;
     }
 
@@ -140,16 +126,10 @@
         .big-disk {
             left: 0;
             top: 0;
-            //width: $big-disk-diameter;
-
             background-image: url("./assets/big-disk.png");
         }
 
         .small-disk {
-            //top: calc($big-disk-diameter / 2 - $small-disk-diameter / 2);
-            //left: calc($big-disk-diameter / 2 - $small-disk-diameter / 2);
-            //width: $small-disk-diameter;
-
             background-image: url("./assets/small-disk.png");
         }
 
