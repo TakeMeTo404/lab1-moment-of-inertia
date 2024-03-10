a872820b06ed2eaf46d9a718884b376027c19179
takemeto404 <takemeto404@gmail.com>
2024-03-11 00:55:59 +0300

    physics add reactive parameters physics

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,58 @@
 <script lang="ts">
     import { tweened } from 'svelte/motion'
-    import { mRange, dRange, dRangePx, containerHeightPx, flywheelDiameterPx, loadDiameterPx } from './const'
+    import { quadInOut } from 'svelte/easing'
+    import {
+        mRange,
+        dRange,
+        dRangePx,
+        n1Range,
+        containerHeightPx,
+        flywheelDiameterPx,
+        variantToI,
+        loadDiameterRangePx
+    } from './const'
     import { width, height, scale } from './sizes'
 
-    type State = 'idle' | 'falling' | 'fall-done'
+    let appState: 'idle' | 'targeting' | 'falling' | 'fall-done' = 'idle'
+    let error: string = ''
 
-    let state: State = 'idle'
+    let variantNumber: number | null
+    let I: number
+    $: I = variantToI[variantNumber ?? 1] ?? variantToI[1]
 
-    let m = mRange[0]
-    let d = (dRange[0] + dRange[1]) / 2
-    let n1 = 3
-    const I = 0.204
-    const g = 9.81
-    const al = .07
+    let m: number | null = null
+    let d: number | null = null
+    let n1: number
 
-    let t: number
-    let n2: number
-    $: {
+    /*$: {
+        if (appState == 'idle' || appState === 'targeting') {
+            const mm: number = m ?? (mRange[0] + mRange[1]) / 2
+            const dd: number = d ?? (dRange[0] + dRange[1]) / 2
+            const n1n1: number = n1 ?? 0
+
+            const mFr = (mm - mRange[0]) / (mRange[1] - mRange[0])
+            const dFr = (dd - dRange[0]) / (dRange[1] - dRange[0])
+
+            const loadDiameterPx = loadDiameterRangePx[0] + (loadDiameterRangePx[1] - loadDiameterRangePx[0]) * mFr
+            const pulleyDiameterPx = dRangePx[0] + (dRangePx[1] - dRangePx[0]) * dFr
+            const loadPositionPx = containerHeightPx - loadDiameterPx - n1n1 * pulleyDiameterPx * Math.PI
+
+            let targetState: PhysicsState = {
+                pulleyDiameterPx,
+                loadDiameterPx,
+                loadPositionPx,
+                systemRotation: -n1n1 - 5
+            }
+
+            appState = 'targeting'
+            physicsStateTweened.set(targetState, { duration: 500 })
+                .then(() => appState = 'idle')
+        }
+    }*/
+
+    let t: number = 5
+    let n2: number = 10
+    /*$: {
         console.group()
         console.log(`m = ${m.toFixed(2)}кг.`)
         console.log(`d = ${d.toFixed(3)}м.`)
@@ -50,7 +86,7 @@
         // console.log('x= ', x)
         // n2 = n1 / (x - 1)
 
-        /* этот checkI получается такой же, как и I */
+        /!* этот checkI получается такой же, как и I *!/
         // const checkI = m * d**2 * t**2 * g / (8 * H * (1 + n1/n2))
         // console.log(`I=${I.toFixed(2)}, checkI=${checkI.toFixed(2)}`)
 
@@ -66,7 +102,7 @@
         // t = 6.4
         // n2 = 91
 
-        /* console.group('Подставляю')
+        /!* console.group('Подставляю')
          console.log(`m = ${m}`)
          console.log(`d = ${d}`)
          console.log(`t = ${t}`)
@@ -78,56 +114,146 @@
          console.group('Получаю')
          const IFinal = m * d**2 * t**2 * g / (8 * H * (1 + n1/n2))
          console.log(`I = ${IFinal}`)
-         console.groupEnd()*/
+         console.groupEnd()*!/
+    }*/
+
+    type PhysicsState = {
+        pulleyDiameterPx: number
+        systemRotation: number
+        loadPositionPx: number
+        loadDiameterPx: number
     }
 
-    let pulleyDiameterPx = dRangePx[0]
-    $: {
-        const fr = (d - dRange[0]) / (dRange[1] - dRange[0])
-        pulleyDiameterPx = dRangePx[0] + (dRangePx[1] - dRangePx[0]) * fr
-    }
+    let currentPulleyDiameterPx: number
+    let currentSystemRotation: number
+    let currentLoadPositionPx: number
+    let currentLoadDiameterPx: number
 
-    let loadBeginFallTopPx = 0
-    let loadEndFallTopPx = 0
-    $: {
-        loadEndFallTopPx = containerHeightPx - loadDiameterPx
-
-        loadBeginFallTopPx = containerHeightPx - (loadDiameterPx + n1 * pulleyDiameterPx * Math.PI)
-    }
-
-    let loadPositionPx: number
-    $: {
-        if (state === 'idle') {
-            loadPositionPx = loadBeginFallTopPx
+    const physicsStateTweened = tweened<PhysicsState>(calculateTargetPhysicsState())
+    physicsStateTweened.subscribe((currentState) => {
+        if ((appState !== 'idle' && appState !== 'targeting') || !currentState) {
+            return
         }
-    }
+
+        currentSystemRotation = currentState.systemRotation
+        currentLoadDiameterPx = currentState.loadDiameterPx
+        currentLoadPositionPx = currentState.loadPositionPx
+        currentPulleyDiameterPx = currentState.pulleyDiameterPx
+    })
 
     let threadTopPx: number = flywheelDiameterPx / 2
     let threadLeftPx: number
     let threadLengthPx: number
-    $: threadLeftPx = flywheelDiameterPx / 2 + pulleyDiameterPx / 2 - 2
-    $: threadLengthPx = loadPositionPx - flywheelDiameterPx / 2
-
-    let disksRotation = 0
+    $: threadLeftPx = flywheelDiameterPx / 2 + currentPulleyDiameterPx / 2 - 2
+    $: threadLengthPx = currentLoadPositionPx - flywheelDiameterPx / 2
 
     let loadFallingTime: number = 0
     let disksRotationsDone: number = 0
     let disksRotationDoneDisplayed: number
     $: disksRotationDoneDisplayed = Math.floor(disksRotationsDone * 4) / 4
 
+    function calculateTargetPhysicsState (): PhysicsState {
+        const mm: number = m ?? (mRange[0] + mRange[1]) / 2
+        const dd: number = d ?? (dRange[0] + dRange[1]) / 2
+        const n1n1: number = n1 ?? 0
+
+        const mFr = (mm - mRange[0]) / (mRange[1] - mRange[0])
+        const dFr = (dd - dRange[0]) / (dRange[1] - dRange[0])
+
+        const loadDiameterPx = loadDiameterRangePx[0] + (loadDiameterRangePx[1] - loadDiameterRangePx[0]) * mFr
+        const pulleyDiameterPx = dRangePx[0] + (dRangePx[1] - dRangePx[0]) * dFr
+        const loadPositionPx = containerHeightPx - loadDiameterPx - n1n1 * pulleyDiameterPx * Math.PI
+
+        return {
+            pulleyDiameterPx,
+            loadDiameterPx,
+            loadPositionPx,
+            systemRotation: -n1n1
+        }
+    }
+
+    const calculateTimeOfMovingTo = (targetState: PhysicsState) => {
+        const loadPositionRangePx = [
+            containerHeightPx - loadDiameterRangePx[1] - n1Range[1] * dRangePx[1] * Math.PI,
+            containerHeightPx - loadDiameterRangePx[0]
+        ] as const
+
+        const delta: PhysicsState = {
+            systemRotation: Math.abs(currentSystemRotation - targetState.systemRotation),
+            pulleyDiameterPx: Math.abs(currentPulleyDiameterPx - targetState.pulleyDiameterPx),
+            loadDiameterPx: Math.abs(currentLoadDiameterPx - targetState.loadDiameterPx),
+            loadPositionPx: Math.abs(currentLoadPositionPx - targetState.loadPositionPx)
+        }
+
+        const frs: PhysicsState = {
+            systemRotation: delta.systemRotation / (n1Range[1] - n1Range[0]),
+            pulleyDiameterPx: delta.pulleyDiameterPx / (dRangePx[1] - dRangePx[0]),
+            loadDiameterPx: delta.loadDiameterPx / (loadDiameterRangePx[1] - loadDiameterRangePx[0]),
+            loadPositionPx: delta.loadPositionPx / (loadPositionRangePx[1] - loadPositionRangePx[0]),
+        }
+
+        const coefficients: PhysicsState = {
+            systemRotation: 1,
+            pulleyDiameterPx: 1,
+            loadDiameterPx: 1,
+            loadPositionPx: 1,
+        }
+
+        console.log(delta)
+        console.log(frs)
+        console.log(Object.entries(coefficients))
+
+        const fr =
+            Object.entries(coefficients)
+                .map(([key, value]) => value * frs[key as keyof PhysicsState])
+                .reduce((acc, c) => acc + c, 0) /
+            Object.values(coefficients).reduce((acc, c) => acc + c, 0)
+        console.log(fr)
+
+        return fr * 3000
+    }
+
+    const updatePhysicsTarget = async () => {
+        appState = 'targeting'
+        const newState = calculateTargetPhysicsState()
+        const duration = calculateTimeOfMovingTo(newState)
+        await physicsStateTweened.set(newState, { duration, easing: quadInOut })
+        appState = 'idle'
+    }
+
     const onClickStart = async () => {
-        state = 'falling'
+        if (appState !== 'idle') {
+            return
+        }
+
+        if (!variantNumber) {
+            return error = 'Введите номер варианта'
+        }
+        if (!m) {
+            return error = 'Введите массу груза m'
+        }
+        if (!d) {
+            return error = 'Введите диаметр шкива d'
+        }
+        if (!n1) {
+            return error = 'Введите количество оборотов n1'
+        }
+
+        appState = 'falling'
+
+        const loadEndFallTopPx = containerHeightPx - currentLoadDiameterPx
+        const loadBeginFallTopPx = containerHeightPx - (currentLoadDiameterPx + n1 * currentPulleyDiameterPx * Math.PI)
 
         const t1Tweened = tweened(0)
+        const H = loadEndFallTopPx - loadBeginFallTopPx
 
         let unsubscribes = [
             t1Tweened.subscribe(t1 => loadFallingTime = t1),
             t1Tweened.subscribe(t1 => {
-                const H = loadEndFallTopPx - loadBeginFallTopPx
-                loadPositionPx = loadBeginFallTopPx + H * t1 * t1 / (t * t)
+                currentLoadPositionPx = loadBeginFallTopPx + H * t1 * t1 / (t * t)
             }),
             t1Tweened.subscribe(t1 => {
-                disksRotation = n1 * t1 * t1 / (t * t)
+                currentSystemRotation = n1 * t1 * t1 / (t * t)
             })
         ]
 
@@ -139,7 +265,7 @@
 
         unsubscribes = [
             t2Tweened.subscribe(t2 => {
-                disksRotation = n1
+                currentSystemRotation = n1
                     + 2 * n1 * t2 / t
                     - (n1 ** 2 * t2 ** 2) / (t ** 2 * n2)
             }),
@@ -153,63 +279,143 @@
         await t2Tweened.set(t2, { duration: t2 * 1000 })
         unsubscribes.forEach(u => u())
 
-        state = 'fall-done'
+        appState = 'fall-done'
     }
 
     const onClickNew = () => {
-        loadPositionPx = loadBeginFallTopPx
-        disksRotation = 0
+        // loadPositionPx = loadBeginFallTopPx
+        // disksRotation = 0
         loadFallingTime = 0
         disksRotationsDone = 0
 
-        state = 'idle'
+        appState = 'idle'
     }
 
-    const onInputM = (e: Event & { currentTarget: HTMLInputElement, target: HTMLInputElement }) => {
-        const value = parseFloat(e.target.value)
-        if (!isNaN(value)) {
-            m = value
+    const onInputVariantNumber = (e: any) => {
+        const value = e.target.value as string
+
+        if (!value) {
+            variantNumber = null
+            return
         }
-    }
-    const onInputD = (e: InputEvent & { currentTarget: HTMLInputElement, target: HTMLInputElement }) => {
-        const value = parseFloat(e.target.value)
-        if (!isNaN(value)) {
-            d = value
+
+        const num = parseInt(value)
+        const valid = num >= 1 && num <= 30 && num % 1 === 0
+        if (!valid) {
+            variantNumber = null
+            error = 'Некорректный номер варианта ' + num
+            return
         }
+
+        error = ''
+        variantNumber = num
     }
-    const onInputN1 = (e: InputEvent & { currentTarget: HTMLInputElement, target: HTMLInputElement }) => {
-        const value = parseFloat(e.target.value)
-        if (!isNaN(value)) {
-            n1 = value
+
+    const onInputM = (e: any) => {
+        const value = e.target.value as string
+
+        if (!value) {
+            m = null
+            return
         }
+
+        const num = parseFloat(value)
+        const valid = num >= mRange[0] && num <= mRange[1]
+        if (!valid) {
+            error = 'Некорректная масса груза m = ' + num
+            m = null
+            return
+        }
+
+        error = ''
+        m = num
+
+        updatePhysicsTarget()
     }
+
+    const onInputD = (e: any) => {
+        const value = e.target.value as string
+
+        if (!value) {
+            d = null
+            return
+        }
+
+        const num = parseFloat(value)
+        const valid = num >= dRange[0] && num <= dRange[1]
+        if (!valid) {
+            d = null
+            error = 'Некорректный диаметр шкива d = ' + num
+            return
+        }
+
+        error = ''
+        d = num
+
+        updatePhysicsTarget()
+    }
+
+    const onInputN1 = (e: any) => {
+        const value = e.target.value as string
+
+        if (!value) {
+            n1 = null as any
+            return
+        }
+
+        const num = parseFloat(value)
+        const valid = num >= n1Range[0] && num <= n1Range[1]
+        if (!valid) {
+            n1 = null as any
+            error = 'Некорректное количество оборотов n1 = ' + num
+            return
+        }
+
+        error = ''
+        n1 = num
+
+        updatePhysicsTarget()
+    }
+
 </script>
 
 <div class="app" style="width: {width}px; height: {height}px; --scale: {$scale};">
     <div class="ui-holder to-left">
         <div class="ui">
-            <div class="experiment-counter">
-                Эксперимент №1
-            </div>
+            <h3 class="experiment-counter">
+                Лабораторная работа №9
+                <!--                Лабораторная работа №{ obj.aa }-->
+            </h3>
+            <section class="variant">
+                <div class="section-element">
+                    <div class="flex">
+                        <span>Номер варианта</span>
+                        <input type="number" max={30} min={1} placeholder="От 1 до 30"
+                               on:input|preventDefault={onInputVariantNumber}/>
+                    </div>
+                </div>
+            </section>
             <section class="input">
                 <div class="section-element">
                     <div class="flex">
                         <span>m – Масса груза (кг)</span>
-                        <input type="number" max={mRange[0]} min={mRange[1]} step=".1" placeholder="Введите m..." on:input={onInputM} />
+                        <input type="number" max={mRange[0]} min={mRange[1]} step=".1" placeholder="Введите m..."
+                               on:input={onInputM}/>
                     </div>
                 </div>
                 <div class="divider"></div>
                 <div class="section-element">
                     <div class="flex">
                         <span>d – Диаметр шкива (м)</span>
-                        <input type="number" max={dRange[0]} min={dRange[1]} step=".05" placeholder="Введите d..." on:input={onInputD} />
+                        <input type="number" max={dRange[0]} min={dRange[1]} step=".05" placeholder="Введите d..."
+                               on:input={onInputD}/>
                     </div>
                 </div>
                 <div class="divider"></div>
                 <div class="section-element">
                     <div class="flex">
                         <span>n1 – Количество оборотов</span>
-                        <input type="number" max="4" min="3" step="1" placeholder="Введите n1..." on:input={onInputN1} />
+                        <input type="number" max="4" min="3" step="1" placeholder="Введите n1..." on:input={onInputN1}/>
                     </div>
                 </div>
                 <div class="divider"></div>
@@ -236,24 +442,33 @@
                     <button class="blue" on:click={onClickNew}>Поднять груз</button>
                 </div>
             </section>
-            <section>
-                <div style="height: 2rem;"></div>
+            <section style="height: 3.5rem;">
+                {#if error}
+                    <div class="error-indicator">{error}</div>
+                {/if}
+                <!--                <div class="error-indicator">Масса должна быть в диапазоне [0.3; 1.0]</div>-->
             </section>
         </div>
     </div>
 
     <div class="physics-container" style="height: {containerHeightPx}px; width: {flywheelDiameterPx}px;">
         <div class="flywheel disk"
-             style="width: {flywheelDiameterPx}px; transform: rotateZ({disksRotation * 360}deg);"></div>
+             style="width: {flywheelDiameterPx}px; transform: rotateZ({currentSystemRotation * 360}deg);"></div>
         <div class="pulley disk"
-             style="width: {pulleyDiameterPx}px; top: {flywheelDiameterPx / 2 - pulleyDiameterPx / 2}px; left: {flywheelDiameterPx / 2 - pulleyDiameterPx / 2}px; transform: rotateZ({disksRotation * 360}deg);"></div>
+             style="width: {currentPulleyDiameterPx}px; top: {flywheelDiameterPx / 2 - currentPulleyDiameterPx / 2}px; left: {flywheelDiameterPx / 2 - currentPulleyDiameterPx / 2}px; transform: rotateZ({currentSystemRotation}deg);"></div>
         <div class="load"
-             style="width: {loadDiameterPx}px; height: {loadDiameterPx}px; left: {threadLeftPx - loadDiameterPx / 2}px; top: {loadPositionPx}px;"></div>
+             style="width: {currentLoadDiameterPx}px; height: {currentLoadDiameterPx}px; left: {threadLeftPx - currentLoadDiameterPx / 2}px; top: {currentLoadPositionPx}px;"></div>
         <div class="thread" style="height: {threadLengthPx}px; top: {threadTopPx}px; left: {threadLeftPx}px"></div>
     </div>
 </div>
 
 <style lang="scss">
+    :global(*) {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
     .app {
         position: fixed;
         top: 50%;
@@ -290,6 +505,8 @@
 
             .experiment-counter {
                 padding-top: 1.5rem;
+                padding-bottom: 0;
+                margin-bottom: 0;
                 font-size: 1.5rem;
                 color: white;
                 text-align: center;
@@ -299,12 +516,16 @@
             section {
                 padding: 0 1rem;
 
-                &.input {
+                &.variant {
                     padding-top: 2rem;
                 }
 
+                &.input {
+                    padding-top: 1.5rem;
+                }
+
                 &.output {
-                    padding-top: 2rem;
+                    padding-top: 1.5rem;
                 }
 
                 .section-element {
@@ -316,6 +537,7 @@
                         display: flex;
                         align-items: baseline;
                         justify-content: space-between;
+                        position: relative;
 
                         span {
                             color: white;
@@ -324,8 +546,8 @@
                         }
 
                         input {
-                            border:0;
-                            outline:0;
+                            border: 0;
+                            outline: 0;
                             -webkit-appearance: none;
                             background: transparent;
                             font-size: 16px;
@@ -341,7 +563,7 @@
 
                             &:focus, &:valid {
                                 border: none;
-                                outline:none!important;
+                                outline: none !important;
                             }
 
                             &::-webkit-outer-spin-button,
@@ -382,6 +604,7 @@
                         border-top-left-radius: 1rem;
                         border-top-right-radius: 1rem;
                     }
+
                     &:last-child {
                         border-bottom-left-radius: 1rem;
                         border-bottom-right-radius: 1rem;
@@ -390,6 +613,13 @@
 
                 .divider {
                     height: 1px;
+                }
+
+                .error-indicator {
+                    padding-top: 1rem;
+                    padding-bottom: 1rem;
+                    font-size: 16px;
+                    color: #e72416;
                 }
             }
         }
--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,39 @@
 export const containerHeightPx = 700
 export const flywheelDiameterPx = 200
-export const loadDiameterPx = 80
 export const mRange = [ .3, 1 ]
 export const dRange = [ .05, .1 ]
 export const dRangePx = [ 30, 50 ]
+export const loadDiameterRangePx = [ 50, 100 ]
+export const n1Range = [ 2, 4 ]
+export const variantToI: Record<number, number> = {
+    1: 2.559,
+    2: 1.173,
+    3: 1.518,
+    4: 2.512,
+    5: 1.722,
+    6: 2.455,
+    7: 0.752,
+    8: 1.306,
+    9: 2.090,
+    10: 0.692,
+    11: 1.972,
+    12: 2.673,
+    13: 1.399,
+    14: 2.903,
+    15: 0.645,
+    16: 2.778,
+    17: 2.070,
+    18: 1.853,
+    19: 1.250,
+    20: 0.696,
+    21: 1.997,
+    22: 1.139,
+    23: 1.845,
+    24: 1.016,
+    25: 0.965,
+    26: 2.895,
+    27: 1.083,
+    28: 2.503,
+    29: 1.210,
+    30: 1.490
+}
